330e6f616e33f266d49fdeb8d73e31c657224573
AymenM <amejri@quantmetry.com>
2023-10-29 11:57:57 +0100

    REF update invoice information extractor

---
+++ invoice_chatbot/utils.py
@@ -0,0 +1,21 @@
+
+from langchain import PromptTemplate
+
+
+qa_template = """Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else.
+Helpful answer:
+"""
+
+def set_qa_prompt():
+    """
+    Prompt template for QA retrieval for each vectorstore
+    """
+    prompt = PromptTemplate(template=qa_template,
+                            input_variables=['context', 'question'])
+    return prompt
